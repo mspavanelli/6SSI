192cbcda693376af55c57e20ca698fcd2eca7279
mspavanelli <mspavanelli@gmail.com>
2016-07-14 23:06:35 -0300

    Links corrigidos

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ angular.module('ssi', [])
 			{
 				"nome": "Comissão Organizadora",
 				"logo": "img/logo_comissao.png",
-				"url": "http://dasiusp.com"
+				"url": ""
 			},
 			{
 				"nome": "DASI",
@@ -117,6 +117,16 @@ angular.module('ssi', [])
 				"url": "https://www.casadocodigo.com.br/"
 			},
 			{
+				"nome": "Mãe Terra",
+				"logo": "img/logo_mae_terra.png",
+				"url": "http://www.maeterra.com.br"
+			},
+			{
+				"nome": "PRCEU",
+				"logo": "img/logo_prceu.png",
+				"url": "http://prceu.usp.br"
+			},
+			{
 				"nome": "EACH",
 				"logo": "img/logo_each.jpg",
 				"url": "http://www5.each.usp.br"
@@ -125,16 +135,6 @@ angular.module('ssi', [])
 				"nome": "USP",
 				"logo": "img/logo_usp.png",
 				"url": "http://www5.usp.br"
-			},
-			{
-				"nome": "PRCEU",
-				"logo": "img/logo_prceu.png",
-				"url": "http://prceu.usp.br"
-			},
-			{
-				"nome": "Mãe Terra",
-				"logo": "img/logo_mae_terra.png",
-				"url": "http://www.maeterra.com.br"
 			}
 		];
 	});
